67014c1a32a293ab63b340aefb6e26a950da0957
faustluck <faustluck91@gmail.com>
2022-05-06 19:09:02 +0300

    auth start

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <input type="button" value="click" @click="loginMe" />
+</template>
+
+<script>
+export default {
+  name: "loginPage",
+  methods: {
+    loginMe() {
+      this.$store.dispatch("login", {});
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
