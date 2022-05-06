0dec41eec7bb52e2672889a65cc9b1dde325d582
faustluck <faustluck91@gmail.com>
2022-05-06 23:46:24 +0300

    +

--- src/utils/voice.js
+++ src/utils/voice.js
@@ -1,6 +1,6 @@
 export default async function submitTextToVoice(text) {
   try {
-    let response = await fetch(`https://api.voicerss.org//?key=19371763f18e48789d6a3741515f4800&hl=en-gb&Nancy&c=MP3&f=16khz_16bit_stereo&src=${text}`)
+    let response = await fetch(`https://api.voicerss.org//?key=19371763f18e48789d6a3741515f4800&hl=en-gb&v=Nancy&c=MP3&f=16khz_16bit_stereo&src=${text}`)
     if (response.ok) {
       let blob = await response.blob()
       let src = URL.createObjectURL(blob)
