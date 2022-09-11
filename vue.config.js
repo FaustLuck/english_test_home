ef147961d53fc6f589cf10950b51da4e859e89eb
faustluck <faustluck91@gmail.com>
2022-09-11 22:21:52 +0300

    add 404.html

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,16 @@ module.exports = defineConfig({
   outputDir: "docs",
   publicPath:
     process.env.NODE_ENV === "production" ? "/english_test_home/" : "/",
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: "public/index.html",
+      filename: "index.html",
+    },
+    "404": {
+      entry: "src/main.js",
+      template: "public/404.html",
+      filename: "404.html"
+    }
+  }
 });
