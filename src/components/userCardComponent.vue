b833c2ceca3e1d89a529ea59aeac38f5fa3eb670
faustluck <faustluck91@gmail.com>
2022-09-18 21:23:16 +0300

    userCardComponent.vue

---
+++ src/components/userCardComponent.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="user"
+       :class="{privileged:user.info.isPrivileged}"
+       @click="()=>this.$emit('changeActiveUser',user.info.displayName)">
+      <span class="user__avatar">
+        <img :src="user.info.photoURL" :alt="user.info.displayName">
+      </span>
+    <span class="user__name">{{ user.info.displayName }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "userCardComponent",
+  props: {
+    user: Object
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.user {
+  display: flex;
+  align-items: center;
+  border-radius: 2rem;
+  box-shadow: 0 0 10px 5px #e9a66a;
+  margin: 1rem;
+  padding: .5rem;
+  background-color: #FFDAB9;
+  text-overflow: clip;
+  cursor: pointer;
+
+  &.privileged {
+    order: -1;
+  }
+
+  &__avatar {
+    margin: .5rem;
+
+
+    & > img {
+      height: 100%;
+      border-radius: 50%;
+    }
+  }
+
+  &__name {
+    text-align: center;
+  }
+}
+</style>
--- src/views/StatisticView.vue
+++ src/views/StatisticView.vue
@@ -1,28 +1,64 @@
 <template>
-  <span>statistic</span>
+  <preloader-component v-if="isLoading"></preloader-component>
+  <section v-else class="users">
+    <user-card-component
+      v-for="user of statistic"
+      :key="user.info.displayName"
+      :user="user"
+      @changeActiveUser="(au)=>this.activeUser=au"
+    >
+    </user-card-component>
+  </section>
 </template>
 
 <script>
 import { mapActions, mapState } from "vuex";
+import PreloaderComponent from "@/components/preloaderComponent";
+import UserCardComponent from "@/components/userCardComponent";
 
 export default {
   name: "StatisticView",
+  components: {UserCardComponent, PreloaderComponent},
+  data() {
+    return {
+      isLoading: true,
+      activeUser: ""
+    };
+  },
   computed: {
     ...mapState("auth", ["isLogin", "isAdmin", "uid"]),
+    ...mapState("statistic", ["statistic"])
+  },
+  watch: {
+    statistic(value) {
+      if (Object.keys(value).length) this.isLoading = false;
+    },
+    isLogin(value) {
+      if (value) {
+        this.getStatistic({
+          uid: this.uid,
+          isAdmin: this.isAdmin
+        });
+      } else {
+        this.$router.replace({name: "test"});
+      }
+    },
+    activeUser(value) {
+      console.log(value);
+    }
   },
   methods: {
     ...mapActions("statistic", ["getStatistic"]),
   },
   async created() {
-    if (!this.isLogin) this.$router.replace({name: "test"});
-    await this.getStatistic({
-      uid: this.uid,
-      isAdmin: this.isAdmin
-    });
+    // if (!this.isLogin) this.$router.replace({name: "test"});
   }
 };
 </script>
 
 <style lang="scss" scoped>
-
+.users {
+  display: flex;
+  flex-direction: column;
+}
 </style>
