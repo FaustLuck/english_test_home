913cc52ea1a3ce831eb710e1c99f6a29f3f9efd1
faustluck <faustluck91@gmail.com>
2022-09-19 20:27:06 +0300

    dateList

--- src/store/statistic.js
+++ src/store/statistic.js
@@ -1,15 +1,35 @@
 import { firebaseRealtime } from "@/main";
 import { get, ref } from "firebase/database";
+import { getDate } from "@/utils";
 
 export const statistic = {
   namespaced: true,
   state: {
     statistic: null,
+    dateList: null,
   },
   mutations: {
     saveStatistic(state, statistic) {
       state.statistic = statistic;
     },
+    createDateList(state, statisticData) {
+      let tmp = {};
+      for (let user in statisticData) {
+        tmp[user] = [];
+        for (let timestamp in statisticData[user].statistic) {
+          let [date, time] = getDate(timestamp);
+          let i = tmp[user].findIndex((el) => el[0] === date);
+          if (i > -1) {
+            tmp[user][i][1].push(time);
+          } else {
+            tmp[user].push([date, [time]]);
+          }
+        }
+        tmp[user].reverse();
+        tmp[user].map(([, time]) => time.reverse());
+      }
+      state.dateList = tmp;
+    }
   },
   actions: {
     async getStatistic({ commit }, { uid, isAdmin }) {
@@ -28,7 +48,20 @@ export const statistic = {
           statisticData = tmp;
         }
         commit("saveStatistic", statisticData);
+        commit("createDateList", statisticData);
       }
     },
+  },
+  getters: {
+    getTest(state) {
+      return function (uid, timestamp) {
+        return state.statistic[uid].statistic[timestamp].test;
+      };
+    },
+    getTimeSpent(state) {
+      return function (uid, timestamp) {
+        return state.statistic[uid].statistic[timestamp].timeSpent;
+      };
+    }
   }
 };
