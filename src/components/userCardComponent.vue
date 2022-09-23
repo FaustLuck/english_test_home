9cafdb7c2a4aee665271bf9821246c84763eecac
faustluck <faustluck91@gmail.com>
2022-09-23 20:58:45 +0300

    Statistic.vue adaptive

--- src/components/userCardComponent.vue
+++ src/components/userCardComponent.vue
@@ -31,6 +31,9 @@ export default {
   background-color: #FFDAB9;
   text-overflow: clip;
   cursor: pointer;
+  @media screen and (max-width: 768px) {
+    margin: 1rem 0;
+  }
 
   &.privileged {
     order: -1;
