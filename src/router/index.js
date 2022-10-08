53a91233bd27bb2c29b15665bcc1303254d40605
faustluck <faustluck91@gmail.com>
2022-10-08 11:00:24 +0300

    renaming chunk

--- src/router/index.js
+++ src/router/index.js
@@ -30,12 +30,12 @@ const routes = [
   {
     path: "/fire-show",
     name: "fire-show",
-    component: () => import(/* webpackChunkName: "FireShowView" */"@/views/AnimationShow.vue")
+    component: () => import(/* webpackChunkName: "AnimationShow" */"@/views/AnimationShow.vue")
   },
   {
     path: "/fail-show",
     name: "fail-show",
-    component: () => import(/* webpackChunkName: "FireShowView" */"@/views/AnimationShow.vue")
+    component: () => import(/* webpackChunkName: "AnimationShow" */"@/views/AnimationShow.vue")
   }
 ]
 
