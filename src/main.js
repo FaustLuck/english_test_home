2b6082ace475381499c577d7098f9bf88a3959d7
faustluck <faustluck91@gmail.com>
2022-10-12 22:19:36 +0300

    todo

--- src/main.js
+++ src/main.js
@@ -31,6 +31,10 @@ function initFirebase() {
 initFirebase();
 createApp(App).use(store).use(router).mount("#app");
 
+//todo cardTestItemComponent для settings
+//todo cardTestItemComponent для test
+//todo cardTestItemComponent для result/statistic
+//todo title component? (position:sticky if isOpen etc.)
 
 //todo семантика?
 //todo вирт. питомец
