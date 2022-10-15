b53e73d0541145c4a8df93264770005b6fbce279
faustluck <faustluck91@gmail.com>
2022-10-15 11:50:58 +0300

    todo

--- src/main.js
+++ src/main.js
@@ -34,7 +34,6 @@ createApp(App).use(store).use(router).mount("#app");
 //todo cardTestItemComponent для settings
 //todo cardTestItemComponent для test
 //todo cardTestItemComponent для result/statistic
-//todo title component? (position:sticky if isOpen etc.)
 
 //todo семантика?
 //todo вирт. питомец
