c33c1c91d9a23ce9b75f319115d28b779c88c64d
faustluck <faustluck91@gmail.com>
2023-02-17 08:20:04 +0300

    remove router rules

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import store from "@/store";
 
 const routes = [
   {
@@ -55,31 +54,31 @@ const router = createRouter({
 });
 
 
-router.beforeEach(async (to, from) => {
-  const isAdmin = store.state.auth.isAdmin;
-  const uid = await store.dispatch("auth/getUID") ?? store.state.auth.uid;
-  if (to.meta.requireAdmin) {
-    if (isAdmin) {
-      return true;
-    } else {
-      if (to.name === "users") {
-        return uid !== "unauthorizedUser" ? {name: "statistic", params: {uid}} : {name: "test"};
-      } else {
-        return {name: "test"};
-      }
-    }
-  }
-  if (to.name === "users") {
-    if (uid === "unauthorizedUser") return {name: "test"};
-    return (uid === to.params.uid || isAdmin) ? true : {name: "statistic", params: {uid}};
-  }
-  if (to.name === "result") {
-    return (store.state.test.answers) ? true : {name: "test"};
-  }
-  if (to.name.includes("show")) {
-    return (from.name === "result") ? true : {name: "test"};
-
-  }
-});
+// router.beforeEach(async (to, from) => {
+//   const isAdmin = store.state.auth.isAdmin;
+//   const uid = await store.dispatch("auth/getUID") ?? store.state.auth.uid;
+//   if (to.meta.requireAdmin) {
+//     if (isAdmin) {
+//       return true;
+//     } else {
+//       if (to.name === "users") {
+//         return uid !== "unauthorizedUser" ? {name: "statistic", params: {uid}} : {name: "test"};
+//       } else {
+//         return {name: "test"};
+//       }
+//     }
+//   }
+//   if (to.name === "users") {
+//     if (uid === "unauthorizedUser") return {name: "test"};
+//     return (uid === to.params.uid || isAdmin) ? true : {name: "statistic", params: {uid}};
+//   }
+//   if (to.name === "result") {
+//     return (store.state.test.answers) ? true : {name: "test"};
+//   }
+//   if (to.name.includes("show")) {
+//     return (from.name === "result") ? true : {name: "test"};
+//
+//   }
+// });
 
 export default router;
