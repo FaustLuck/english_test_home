9ce49c3a156b08f336da7309c88de4a539165e6e
faustluck <faustluck91@gmail.com>
2023-02-18 22:11:19 +0300

    rebuild StatisticView.vue

--- src/views/StatisticView.vue
+++ src/views/StatisticView.vue
@@ -1,53 +1,30 @@
 <template>
-  <preloader-component v-if="isLoading"></preloader-component>
-  <div v-else-if="isStaticExists">
+  <div class="statistic">
     <date-list-component
-      v-for="[date,timeArray] of dateList[uid]"
-      :key="date"
-      :uid="uid"
-      :date="date"
-      :time-array="timeArray"
+      v-for="(count,datestamp) in dateList"
+      :key="datestamp"
+      :count="count"
+      :datestamp="datestamp"
     >
     </date-list-component>
   </div>
-  <h1 v-else>Пользователь не найден...</h1>
 </template>
 
 <script>
 
 import { defineAsyncComponent } from "vue";
-import { mapActions, mapState } from "vuex";
-
+import { mapState } from "vuex";
 export default {
   name: "StatisticUserView",
   components: {
-    preloaderComponent: defineAsyncComponent(() => import("@/components/preloaderComponent")),
     dateListComponent: defineAsyncComponent(() => import("@/components/dateListComponent")),
   },
   props: {
-    uid: String
-  },
-  data() {
-    return {
-      isLoading: true,
-      isStaticExists: null
-    };
+    sub: String
   },
   computed: {
     ...mapState("statistic", ["dateList"]),
-    ...mapState("auth", ["sub"])
-  },
-  watch: {
-    isStaticExists() {
-      this.isLoading = false;
-    }
-  },
-  methods: {
-    ...mapActions("statistic", ["getDateList"])
-  },
-  async created() {
-    this.isStaticExists = await this.getDateList({sub: this.sub});
-    if (this.isStaticExists) await this.requestTimer();
+
   }
 };
 </script>
