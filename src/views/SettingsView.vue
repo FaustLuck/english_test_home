57e38a8dafcfcb1594d3df75dbbf74c0820b5815
faustluck <faustluck91@gmail.com>
2023-02-20 13:21:20 +0300

    fix SettingsView.vue

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -3,18 +3,18 @@
   <div v-else>
     <div class="time">
       <span>Ограничение по времени: </span>
-      <span>{{ settings.timer.min }}</span> : <span>{{ settings.timer.sec }}</span>
+      <span>{{ min }}</span> : <span>{{ sec }}</span>
     </div>
     <div class="variants">
       <span>Количество вариантов ответов: </span>
-      <span>{{ settings.variants }}</span>
+      <span>{{ variants }}</span>
     </div>
     <div class="container">
       <test-difficult-component
         v-for="difficult of orderDifficult"
         :key="difficult"
         :difficult="difficult"
-        :part-answers="settings.dictionary[difficult]"
+        :part-answers="dictionary[difficult]"
       ></test-difficult-component>
     </div>
   </div>
@@ -32,19 +32,38 @@ export default {
   },
   data() {
     return {
-      isLoading: true
+      isLoading: true,
+      min: 0,
+      sec: 0
     };
   },
+  watch: {
+    timer(value) {
+      if (value) {
+        this.timeToString(value);
+        this.isLoading = false;
+      }
+    }
+  },
   computed: {
-    ...mapState("auth", ["isAdmin"]),
-    ...mapState("settings", ["settings"]),
+    ...mapState("settings", ["timer", "dictionary", "limits", "variants"]),
+    ...mapState("auth", ["sub"]),
     ...mapState(["orderDifficult"])
   },
   methods: {
-    ...mapActions("settings", ["requestSettings"])
+    ...mapActions("settings", ["getSettings"]),
+    timeToString(time) {
+      this.sec = (time % 60).toString().padStart(2, "0");
+      this.min = (time - this.sec) / 60;
+    }
   },
-  async created() {
-    this.isLoading = !(await this.requestSettings());
+  async mounted() {
+    if (!this.timer) {
+      await this.getSettings({sub: this.sub});
+    } else {
+      this.timeToString(this.timer);
+      this.isLoading = false;
+    }
   }
 };
 </script>
