dca53b5872491532e97ad7ef9d0f7b10dbcc3281
faustluck <faustluck91@gmail.com>
2023-02-20 13:20:52 +0300

    delete FB

--- src/main.js
+++ src/main.js
@@ -3,32 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCLchFcQWRGJxElVnI0Cv7JGgAvrC8yDZ4",
-  authDomain: "english-test-home.firebaseapp.com",
-  projectId: "english-test-home",
-  storageBucket: "english-test-zhome.appspot.com",
-  messagingSenderId: "223596466893",
-  appId: "1:223596466893:web:bafc299ab7409de7cccf84"
-};
-
-let app = null;
-export let firebaseAuth = null;
-export let firebaseRealtime = null;
-
-function initFirebase() {
-  if (!app) {
-    app = initializeApp(firebaseConfig);
-    firebaseAuth = getAuth(app);
-    firebaseRealtime = getDatabase(app);
-  }
-}
-
-initFirebase();
 createApp(App).use(store).use(router).mount("#app");
 
 //todo cardTestItemComponent для settings
