0e86ca89b502ce58eac8911f8352ad815e06a437
faustluck <faustluck91@gmail.com>
2023-03-09 03:10:00 +0300

    add sub

--- src/components/startButtonComponent.vue
+++ src/components/startButtonComponent.vue
@@ -12,6 +12,7 @@ export default {
   },
   computed: {
     ...mapState("test", ["isTesting"]),
+    ...mapState("auth", ["sub"]),
     ...mapState(["mode"]),
     title() {
       return (this.isTesting) ? "Завершить тест" : "Начать тест";
@@ -25,7 +26,7 @@ export default {
       if (!this.isTesting) {
         if (this.mode !== "test") this.$router.replace({name: "test"});
         this.setLoading(true);
-        await this.getTest({sub: ""});
+        await this.getTest({sub: this.sub});
         this.changeTestStatus(true);
         this.setLoading(false);
       } else {
