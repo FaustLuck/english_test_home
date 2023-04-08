0a5a3e922e4c45c959b5d6acadb5a2b9e46561e6
faustluck <faustluck91@gmail.com>
2023-04-08 17:45:25 +0300

    history types

--- src/types/history.d.ts
+++ src/types/history.d.ts
@@ -1,7 +1,34 @@
+import { TestItem } from "@/types/test";
+
 export interface User {
   key: string;
   name: string;
   picture: string;
   privileged: boolean;
   sub: string;
+}
+
+export interface DateList {
+  [index: number]: { counter: number };
+
+  [index: number]: TestInfo;
+}
+
+export interface TestInfo {
+  correct: number;
+  question: number;
+  timeLeft: number;
+  timeSpent: number;
+}
+
+export interface Statistic {
+  [index: number]: HistoryRecord;
+}
+
+export interface HistoryRecord {
+  [index: number]: DifficultOfTEst;
+}
+
+export interface DifficultOfTEst {
+  [index: string]: TestItem[];
 }
