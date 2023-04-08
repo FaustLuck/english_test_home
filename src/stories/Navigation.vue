95efda1adbc3b25b97c97b1ac047517768d9e12f
faustluck <faustluck91@gmail.com>
2023-04-09 00:48:45 +0300

    navigation drawer

---
+++ src/stories/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-navigation-drawer :temporary="drawer" elevation="5" color="default">
+    <v-list>
+      <v-list-item prepend-icon="mdi mdi-cog-outline" title="Настройки" value="Settings"/>
+      <v-list-item prepend-icon="mdi mdi-content-save-outline" title="История" value="History"/>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script>
+export default {
+  props: {
+    drawer: Boolean
+  }
+};
+</script>
