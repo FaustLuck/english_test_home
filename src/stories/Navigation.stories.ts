ab589e766cdded455a2bf7d4d50e14f22377faeb
faustluck <faustluck91@gmail.com>
2023-04-09 01:25:56 +0300

    navbar

---
+++ src/stories/Navigation.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import NavigationComponent from "@/stories/Navigation.vue";
+
+const meta = {
+  title: "App/Navigation",
+  component: NavigationComponent,
+  tags: ["autodocs"],
+  args: {
+    isHidden: false
+  },
+  render: (args) => ({
+    components: { NavigationComponent },
+    setup(){
+      return {args}
+    },
+    template: `
+	    <v-layout>
+		    <navigation-component v-bind="args"/>
+	    </v-layout>
+    `
+  })
+}satisfies Meta<typeof NavigationComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>
+
+export const Open: Story = {};
--- src/stories/Navigation.vue
+++ src/stories/Navigation.vue
@@ -1,15 +1,11 @@
 <template>
-  <v-navigation-drawer :temporary="drawer" elevation="5" color="default">
+  <v-navigation-drawer :temporary="isHidden" elevation="5" color="default">
     <v-list>
       <v-list-item prepend-icon="mdi mdi-cog-outline" title="Настройки" value="Settings"/>
       <v-list-item prepend-icon="mdi mdi-content-save-outline" title="История" value="History"/>
     </v-list>
   </v-navigation-drawer>
 </template>
-<script>
-export default {
-  props: {
-    drawer: Boolean
-  }
-};
+<script setup lang="ts">
+const props = defineProps<{ isHidden: boolean }>();
 </script>
