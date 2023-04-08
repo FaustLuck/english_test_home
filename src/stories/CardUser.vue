a14503058f3a01327e23e0727a6fe13de8b35dba
faustluck <faustluck91@gmail.com>
2023-04-08 03:01:17 +0300

    CardUser component

---
+++ src/stories/CardUser.vue
@@ -0,0 +1,27 @@
+<template>
+  <template v-if="!props.user">
+    <v-card class="d-flex align-center my-3" color="default" elevation="5" rounded="lg">
+      <v-card-item>
+        <v-avatar color="surface-variant" size="80"></v-avatar>
+      </v-card-item>
+      <line-loading-component/>
+    </v-card>
+  </template>
+
+  <template v-else>
+    <v-card class="d-flex align-center my-3" color="default" elevation="5" rounded="lg">
+      <v-card-item>
+        <v-avatar class="fill-height" :image="user.picture" size="80"></v-avatar>
+      </v-card-item>
+      <v-card-text class="pa-6">{{ user.name }}</v-card-text>
+    </v-card>
+  </template>
+</template>
+
+<script setup lang="ts">
+import { User } from "@/types/statistic";
+
+import LineLoadingComponent from "@/stories/LineLoading.vue";
+
+const props = defineProps<{ user?: User }>();
+</script>
