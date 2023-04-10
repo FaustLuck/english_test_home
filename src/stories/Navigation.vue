1c4905a029843b210c39161acb1839e2930f14ca
faustluck <faustluck91@gmail.com>
2023-04-10 03:57:29 +0300

    add navigate in user

--- src/stories/Navigation.vue
+++ src/stories/Navigation.vue
@@ -5,12 +5,24 @@
           color="default"
           @click="$emit('toExpand')"
   >
-      <v-list-item nav prepend-icon="mdi mdi-cog-outline" title="Настройки" value="Settings" to="/settings"/>
-      <v-list-item nav prepend-icon="mdi mdi-content-save-outline" title="История" value="History" to="/users"/>
+    <v-list-item nav prepend-icon="mdi mdi-cog-outline" title="Настройки" value="Settings" to="/settings"/>
+    <v-list-item nav prepend-icon="mdi mdi-content-save-outline" title="История" value="History" to="/users"/>
+
+    <template v-if="users.length>1">
+      <v-list-item
+              nav
+              v-for="user of users"
+              :key="user.key"
+              :title="user.name"
+              :value="user.name"
+              :prepend-avatar="user.picture"
+              :to="`/history${user.sub}`"
+      ></v-list-item>
+    </template>
 
     <template v-slot:append v-if="!isCollapsed">
       <v-btn
-        class="float-end"
+              class="float-end"
               variant="text"
               icon="mdi mdi-chevron-left"
               @click.stop="$emit('toCollapse')"
@@ -21,5 +33,9 @@
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { useHistoryStore } from "@/store/history";
+
 const props = defineProps<{ isCollapsed: boolean }>();
+const { users } = storeToRefs(useHistoryStore());
 </script>
