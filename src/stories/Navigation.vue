354a21e723ebda0ffadd938277f7a493bb5c83b5
faustluck <faustluck91@gmail.com>
2023-04-10 12:04:42 +0300

    users list

--- src/stories/Navigation.vue
+++ src/stories/Navigation.vue
@@ -4,21 +4,31 @@
           elevation="5"
           color="default"
           @click="$emit('toExpand')"
+          width="350"
   >
-    <v-list-item nav prepend-icon="mdi mdi-cog-outline" title="Настройки" value="Settings" to="/settings"/>
     <v-list-item nav prepend-icon="mdi mdi-content-save-outline" title="История" value="History" to="/users"/>
 
-    <template v-if="users.length>1">
-      <v-list-item
-              nav
-              v-for="user of users"
-              :key="user.key"
-              :title="user.name"
-              :value="user.name"
-              :prepend-avatar="user.picture"
-              :to="`/history${user.sub}`"
-      ></v-list-item>
+    <template v-if="users.length>1 && !isCollapsed">
+      <v-list>
+        <v-list-group>
+          <template v-slot:activator="{props}">
+            <v-list-item nav v-bind="props" title="Пользователи" prepend-icon="mdi mdi-account-circle"/>
+          </template>
+
+          <v-list-item
+                  nav
+                  v-for="user of users"
+                  :key="user.key"
+                  :title="user.name"
+                  :value="user.name"
+                  :prepend-avatar="user.picture"
+                  :to="`/history${user.sub}`"
+          />
+        </v-list-group>
+      </v-list>
     </template>
+
+    <v-list-item nav prepend-icon="mdi mdi-cog-outline" title="Настройки" value="Settings" to="/settings"/>
 
     <template v-slot:append v-if="!isCollapsed">
       <v-btn
@@ -38,4 +48,5 @@ import { useHistoryStore } from "@/store/history";
 
 const props = defineProps<{ isCollapsed: boolean }>();
 const { users } = storeToRefs(useHistoryStore());
+
 </script>
