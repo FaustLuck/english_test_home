e75178a64bdca7fd3f5b046ad0334d7bb4c54b14
faustluck <faustluck91@gmail.com>
2023-04-11 11:36:33 +0300

    refactor types

--- src/types/test.d.ts
+++ src/types/test.d.ts
@@ -1,14 +1,14 @@
-export interface TestItem {
-  key: string;
+export interface ResultItem {
   question: string;
-  answer?: string[];
-  choice?: string;
-}
-
-export interface ResultItem extends TestItem{
   answer: string;
+  choice?: string;
 }
 
 export interface Result {
   [index: string]: ResultItem[];
+}
+
+export interface TestItem extends ResultItem {
+  key: string;
+  answer?: string[];
 }
