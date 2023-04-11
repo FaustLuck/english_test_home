cf6efc9309271db0ac8842cfa8aaceedeaa7d01a
faustluck <faustluck91@gmail.com>
2023-04-11 10:06:41 +0300

    cardResultItem & story

---
+++ src/stories/CardResultItem.stories.ts
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import CardResultItemComponent from "@/stories/CardResultItem.vue";
+
+const meta = {
+  title: "UI/CardResult",
+  component: CardResultItemComponent,
+  tags: ["autodocs"]
+}satisfies Meta<typeof CardResultItemComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Right: Story = {
+  args: {
+    item: { key: "rightItem", answer: "Пять", choice: "Пять", question: "Five" }
+  }
+};
+
+export const WrongWithChoice: Story = {
+  args: {
+    item: {
+      key: "wrongItemWithChoice",
+      answer: "Семь",
+      choice: "Кто",
+      question: "Seven"
+    }
+  }
+};
+
+export const wrongItemWithChoice: Story = {
+  args: {
+    item: {
+      key: "wrongItemWithChoice",
+      answer: "Семь",
+      choice: "Кто",
+      question: "Seven"
+    }
+  }
+};
+
