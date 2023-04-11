39c1d5972a889724dfd3133be38ab0a241f98b99
faustluck <faustluck91@gmail.com>
2023-04-11 11:39:16 +0300

    CardResultInfo.stories.ts

---
+++ src/stories/CardResultInfo.stories.ts
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import CardResultInfoComponent from "@/stories/CardResultInfo.vue";
+import { useTestStore } from "@/store/test";
+import { Result } from "@/types/test";
+import { useAuthStore } from "@/store/auth";
+
+
+const meta = {
+  title: "UI/Cards/ResultInfo",
+  component: CardResultInfoComponent,
+  tags: ["autodocs"],
+  args: {
+    timestamp: 1681196340000,
+    correct: 3
+  },
+  render: (args) => ({
+    components: { CardResultInfoComponent },
+    setup() {
+      useTestStore().$patch({
+        timeSpent: 100,
+        timeLeft: 100
+      });
+      return { args };
+    },
+    template: `
+			<card-result-info-component v-bind="args"/>`
+  })
+}satisfies Meta<typeof CardResultInfoComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Loading: Story = {};
+
+const result: Result = {
+  "easy": [
+    {
+      "answer": "Мама",
+      "choice": "Мама",
+      "question": "Mother"
+    },
+    {
+      "answer": "Он",
+      "choice": "Он",
+      "question": "He"
+    },
+    {
+      "answer": "Африка",
+      "choice": "Африка",
+      "question": "Africa"
+    }
+  ],
+  "hard": [
+    {
+      "answer": "She is Tanya",
+      "choice": "She is fine, thanks",
+      "question": "Who is she?"
+    }
+  ]
+};
+
+export const Show = {
+  args: {
+    result
+  }
+};
