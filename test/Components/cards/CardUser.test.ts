bb72bb444adc149d1172c39e38bbbaea517762c4
faustluck <faustluck91@gmail.com>
2023-04-13 02:19:02 +0300

    CardUser test

---
+++ test/Components/cards/CardUser.test.ts
@@ -0,0 +1,40 @@
+import CardUser from "@/stories/cards/CardUser.vue";
+import { mountWrapper } from "../../mountWithVuetify";
+import { VueWrapper } from "@vue/test-utils";
+import { expect } from "vitest";
+import { User } from "@/types/history";
+
+const user: User = {
+  key: "key",
+  name: "userName",
+  picture: "someLink",
+  privileged: false,
+  sub: "sub"
+};
+
+describe("CardUser", () => {
+  let wrapper: VueWrapper<any>;
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  test("Отрисовка, если списка пользователей нет", () => {
+    wrapper = mountWrapper({ component: CardUser });
+    expect(wrapper.text().search("Loading") > -1).toBe(true);
+  });
+
+  test("Отрисовка, если список пользователей доступен", () => {
+    wrapper = mountWrapper({ component: CardUser, props: { user } });
+    expect(wrapper.text().search(user.name) > -1).toBe(true);
+    expect(wrapper.find("img").attributes().src).toBe(user.picture);
+    expect(wrapper.find('.v-card').attributes().privileged).toBeUndefined()
+  });
+  
+  test("Применение класса к карточке привилегированного пользователя", async () => {
+    wrapper = mountWrapper({ component: CardUser, props: { user } });
+    await wrapper.setProps({ privileged: true });
+    expect(wrapper.find('.v-card').attributes().privileged).toBeDefined()
+  });
+
+});
