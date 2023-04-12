45692c52c854942e5ecdf03e5e07eb4b67901936
faustluck <faustluck91@gmail.com>
2023-04-13 01:59:09 +0300

    different mounting

--- test/mountWithVuetify.ts
+++ test/mountWithVuetify.ts
@@ -4,7 +4,22 @@ import vuetify from "../src/plugins/vuetify";
 import { createTestingPinia } from "@pinia/testing";
 
 
-export function createWrapper({ component, props = {} }: any, initialState: any = {}) {
+export function mountWrapper({ component, props = {} }: any, initialState: any = {}) {
+  return mount(component, {
+    props,
+    global: {
+      plugins: [
+        vuetify,
+        createTestingPinia({
+          stubActions:false,
+          initialState
+        })
+      ]
+    }
+  });
+}
+
+export function shallowMountWrapper({ component, props = {} }: any, initialState: any = {}) {
   return shallowMount(component, {
     props,
     global: {
