12e55ef7e772bd24323bde7d597738eafd351519
faustluck <faustluck91@gmail.com>
2023-04-12 16:44:42 +0300

    Timer.test.ts

---
+++ test/Components/Timer.test.ts
@@ -0,0 +1,42 @@
+import { afterEach, beforeEach, describe, expect } from "vitest";
+import Timer from "@/stories/Timer.vue";
+import { createWrapper } from "../mountWithVuetify";
+import { VueWrapper } from "@vue/test-utils";
+import { useTestStore } from "../../src/store/test";
+import { nextTick } from "vue";
+
+describe("Timer", () => {
+  let wrapper: VueWrapper<any>;
+
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+    vi.useRealTimers();
+  });
+
+  test("Секунды правильно переводятся в формат мм:сс", () => {
+    wrapper = createWrapper(Timer, { test: { timeLeft: 65 } });
+    expect(wrapper.text()).toBe("1:05");
+  });
+
+  test("При монтировании таймера в pinia записывается его ID", async () => {
+    wrapper = createWrapper(Timer, { test: { timer: 10 } });
+    await nextTick();
+    expect(useTestStore().timeLeft).toBe(10);
+    expect(useTestStore().timerID).not.toBeUndefined();
+  });
+
+  test("Обновление оставшегося времени происходит каждую секунду", async () => {
+    wrapper = createWrapper(Timer, { test: { timer: 10 } });
+    vi.advanceTimersByTime(2 * 1000);
+    await nextTick();
+    expect(useTestStore().timeLeft).toBe(8);
+
+    vi.advanceTimersByTime(5 * 1000);
+    await nextTick();
+    expect(useTestStore().timeLeft).toBe(3);
+  });
+});
