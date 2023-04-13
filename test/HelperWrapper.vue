efc46f1e6e30937715956e9d5d5585692a99db95
faustluck <faustluck91@gmail.com>
2023-04-14 02:09:31 +0300

    helper creator wrapper

---
+++ test/HelperWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-layout>
+      <slot>{{ childComponent}}</slot>
+    </v-layout>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: "HelperWrapper",
+  props: {
+    childComponent: Object
+  }
+};
+</script>
