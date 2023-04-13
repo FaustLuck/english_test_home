3d6941d2bc9f7c4f7a0553c69daad92a50be753a
faustluck <faustluck91@gmail.com>
2023-04-14 02:07:47 +0300

    upd helper

--- test/mountWithVuetify.ts
+++ test/mountWithVuetify.ts
@@ -2,16 +2,17 @@ import { mount } from "@vue/test-utils";
 import vuetify from "../src/plugins/vuetify";
 // @ts-ignore
 import { createTestingPinia } from "@pinia/testing";
+import { StateTree } from "pinia";
 
 
-export function mountWrapper({ component, props = {} }: any, initialState: any = {}) {
+export function mountWrapper({ component, props = {}, slots = {} }: any, initialState: StateTree = {}) {
   return mount(component, {
     props,
+    slots,
     global: {
       plugins: [
         vuetify,
         createTestingPinia({
-          stubActions:false,
           initialState
         })
       ]
