1982538676bab5c7d25438b9f2e5425b64af4c0a
faustluck <faustluck91@gmail.com>
2023-04-14 16:46:53 +0300

    Header.test.ts --full

--- test/Components/Header.test.ts
+++ test/Components/Header.test.ts
@@ -3,8 +3,11 @@ import { mountWrapper } from "../mountWithVuetify";
 import { h, nextTick } from "vue";
 import Header from "@/stories/Header.vue";
 import HelperWrapper from "../HelperWrapper.vue";
-import { useAuthStore } from "../../src/store/auth";
 import { beforeEach, expect } from "vitest";
+import { useAuthStore } from "@/store/auth";
+import { useLoadingStore } from "@/store/loading";
+import { useCommonStore } from "../../src/store/common";
+import { fetchHelperGet } from "../fetchHelpers";
 
 const component = HelperWrapper;
 
@@ -64,17 +67,31 @@ describe("Header", () => {
     expect(icons[0].html().search("mdi-menu") > -1).toBe(true);
   });
 
-  test("Смена надписи на кнопке", async () => {
+  test("Клик по кнопке отправляет запрос на тест", async () => {
+    global.fetch=fetchHelperGet()
     expect(wrapper.text().search("Начать тест") > -1).toBe(true);
     expect(wrapper.text().search("Завершить тест") > -1).toBe(false);
+    expect(useLoadingStore().isLoading).toBe(false);
+    expect(wrapper.findAll(".v-progress-circular").length).toBe(0);
     await wrapper.find("button:not([id='google'])").trigger("click");
-    await nextTick();
-    expect(wrapper.text().search("Начать тест") > -1).toBe(false);
-    expect(wrapper.text().search("Завершить тест") > -1).toBe(true);
+    expect(global.fetch).toBeCalledWith(`${import.meta.env.VITE_dev}/test/`);
+    expect(useLoadingStore().isLoading).toBe(true);
+    expect(wrapper.findAll(".v-progress-circular").length).toBe(1);
   });
 
-  //todo mock роутера написан верно
-  //проверить его на смену путей
+  test("Если маршрут отличен от result и test, пушит новый путь", async () => {
+    useCommonStore().mode = "somePath";
+    global.fetch=fetchHelperGet()
+    await wrapper.find("button:not([id='google'])").trigger("click");
+    expect(mockedUseRouter.replace).not.toHaveBeenCalledTimes(0);
+    expect(mockedUseRouter.push).toHaveBeenCalledWith({name:'test'});
+  });
 
-  //todo замокать fetch
+  test("Если маршрут result, делает замену пути",async ()=>{
+    useCommonStore().mode = "result";
+    global.fetch=fetchHelperGet()
+    await wrapper.find("button:not([id='google'])").trigger("click");
+    expect(mockedUseRouter.replace).toHaveBeenCalledWith({name:'test'});
+    expect(mockedUseRouter.push).not.toHaveBeenCalledTimes(0);
+  })
 });
