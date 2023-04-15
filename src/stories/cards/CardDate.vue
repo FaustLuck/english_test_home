b895b0e5e784b320c2947c8d2544c5c171f15ab3
faustluck <faustluck91@gmail.com>
2023-04-16 02:46:49 +0300

    check timeList value

--- src/stories/cards/CardDate.vue
+++ src/stories/cards/CardDate.vue
@@ -28,6 +28,7 @@ import { computed } from "vue";
 import { getDate } from "@/utils/getDate";
 import LineLoadingComponent from "@/stories/bricks/LineLoading.vue";
 import { useHistoryStore } from "@/store/history";
+import { TimeList } from "@/types/history";
 
 interface dateCardProps {
   datestamp?: number,
@@ -38,12 +39,19 @@ interface dateCardProps {
 const { datestamp, counter, sub } = defineProps<dateCardProps>();
 const date = computed(() => datestamp && getDate(datestamp)[0]);
 const timeList = computed(() => {
-  if (sub && datestamp) return useHistoryStore().dateList[sub][datestamp];
+  if (!(sub && datestamp)) return;
+  const inner = useHistoryStore().dateList[sub][datestamp];
+  const output: TimeList = {};
+  for (const key in inner) {
+    if (key === "counter") continue;
+    output[key] = inner[key];
+  }
+  return output;
 });
 
 async function getTime() {
   const keys = (timeList.value && Object.keys(timeList.value).length) ?? 0;
-  if (keys > 1) return;
+  if (keys > 0) return;
   if (sub && datestamp) await useHistoryStore().getTimeList(sub, datestamp);
 }
 
