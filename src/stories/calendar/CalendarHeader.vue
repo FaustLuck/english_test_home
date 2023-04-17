be752a21ec37adf7aa6be58aecbfe0b2b7522325
faustluck <faustluck91@gmail.com>
2023-04-17 19:25:16 +0300

    calendar header

---
+++ src/stories/calendar/CalendarHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card class="d-flex justify-center align-center mb-3" elevation="5" color="transparent">
+    <v-btn icon="mdi mdi-chevron-left" elevation="5" color="transparent" @click="$emit('decrement')"/>
+    <v-card-title>{{ year }}</v-card-title>
+    <v-btn icon="mdi mdi-chevron-right" elevation="5" color="transparent" @click="$emit('increment')"/>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ year: number }>();
+</script>
