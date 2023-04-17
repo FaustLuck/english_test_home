a1b7d6da4bc0b97a340ff0aafa6ed7cb00e191d1
faustluck <faustluck91@gmail.com>
2023-04-18 02:28:10 +0300

    long render issue fix

--- src/stories/calendar/Month.vue
+++ src/stories/calendar/Month.vue
@@ -1,6 +1,6 @@
 <template>
   <v-sheet elevation="5" class="d-flex align-center flex-column h-100 pa-3" rounded="lg" color="transparent">
-    <v-card-title class="pa-0 text-capitalize">{{ monthName}}</v-card-title>
+    <v-card-title class="pa-0 text-capitalize">{{ monthName }}</v-card-title>
     <table>
       <thead>
       <tr>
@@ -13,20 +13,20 @@
       </thead>
       <tbody>
 
-      <template v-if="isLoaded">
-        <tr v-for="week of month" :key="week">
-          <td class="text-center pa-1" v-for="day of week">
-            <v-btn v-if="day" variant="text" icon="" density="comfortable">
-              {{ day }}
-            </v-btn>
+      <template v-if="props.loading">
+        <tr v-for="i of 5" :key="`${props.monthIndex}_${i}`">
+          <td class="text-center" colspan="7">
+            <LineLoading/>
           </td>
         </tr>
       </template>
 
       <template v-else>
-        <tr v-for="i of 5" :key="i">
-          <td class="text-center" colspan="7">
-            <LineLoading/>
+        <tr v-for="(week,i) of month" :key="`week_${i}`">
+          <td class="text-center pa-1" v-for="day of week" :key="`week_${i}_${day}`">
+            <v-btn v-if="day>0" variant="text" icon="" density="comfortable" :disabled="getTests(day).length===0">
+              {{ day }}
+            </v-btn>
           </td>
         </tr>
       </template>
@@ -37,13 +37,17 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import LineLoading from "@/stories/bricks/LineLoading.vue";
+import { useHistoryStore } from "@/store/history";
+
+const start = ref(0);
 
 interface CardMonthProps {
   monthIndex: number;
   year: number;
-  isLoaded: boolean;
+  loading: boolean;
+  sub: string;
 }
 
 const monthName = computed(() => new Date(props.year, props.monthIndex).toLocaleString("ru-RU", { month: "long" }));
@@ -52,37 +56,39 @@ const daysName = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
 
 const props = defineProps<CardMonthProps>();
 
-const firstDayIndex = computed(() => {
-  return getDayIndex(1);
-});
+const month = computed(createTable);
 
-const maxDays = computed(() => {
-  return new Date(props.year, props.monthIndex + 1, 0).getDate();
-});
-
-const maxWeeks = computed(() => {
-  let weeks = 0;
-  let restOfWeek = 0;
-  if (firstDayIndex.value > 0) {
-    ++weeks;
-    restOfWeek = 7 - firstDayIndex.value;
-  }
-  weeks += Math.ceil((maxDays.value - restOfWeek) / 7);
-  return weeks;
-});
-
-const month = computed(() => {
-  let month = new Array(maxWeeks.value);
-  month[0] = [];
+function createTable() {
+  const maxDays = new Date(props.year, props.monthIndex + 1, 0).getDate();
+  const output: number[][] = [];
+  output[0] = [];
   let week = 0;
-  for (let day = 1; day <= maxDays.value; day++) {
+  for (let day = 1; day <= maxDays; day++) {
     const index = getDayIndex(day);
-    if (index === 0) month[week] = [];
-    month[week][index] = day;
-    if (index === 6) week++;
+    if (day === 1) fillEmptyDays(output[0], index);
+    if (index === 0) output[week] = [];
+    output[week][index] = day;
+    if (index === 6) ++week;
   }
-  return month;
-});
+
+  return output;
+}
+
+function fillEmptyDays(week: number[], index: number) {
+  for (let i = 0; i < index; i++) {
+    week[i] = -i - 1;
+  }
+
+}
+
+function getTests(day: number) {
+  const start = getStartDayTimestamp(day);
+  return useHistoryStore().getTestsOfDay(props.sub, props.year, start);
+}
+
+function getStartDayTimestamp(day: number) {
+  return new Date(props.year, props.monthIndex, day).getTime();
+}
 
 function getDayIndex(day: number) {
   let weekDay = new Date(props.year, props.monthIndex, day).getDay();
