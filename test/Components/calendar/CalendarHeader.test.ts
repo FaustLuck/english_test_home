ed7e384524a82ce235e84947df6680774133d2f3
faustluck <faustluck91@gmail.com>
2023-04-18 02:51:24 +0300

    CalendarHeader.test.ts

---
+++ test/Components/calendar/CalendarHeader.test.ts
@@ -0,0 +1,57 @@
+import { VueWrapper } from "@vue/test-utils";
+import { mountWrapper } from "../../mountWithVuetify";
+import CalendarHeader from "@/stories/calendar/CalendarHeader.vue";
+import { expect } from "vitest";
+
+const component = CalendarHeader;
+
+describe("CalendarHeader", () => {
+  let wrapper: VueWrapper;
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  test("Проверка отрисовки", () => {
+    wrapper = mountWrapper({
+      component, props: {
+        year: 2022,
+        min: 2021,
+        max: 2023
+      }
+    });
+    expect(wrapper.text().search("2022") > -1).toBe(true);
+    expect(wrapper.html().search("mdi-chevron-left") > -1).toBe(true);
+    expect(wrapper.html().search("mdi-chevron-right") > -1).toBe(true);
+  });
+
+  test("Кнопки влево/вправо заблокированы, если счетчик достиг минимального/максимального порога", () => {
+    wrapper = mountWrapper({
+      component, props: {
+        year: 2022,
+        min: 2022,
+        max: 2022
+      }
+    });
+    const buttons = wrapper.findAll(".v-btn");
+    buttons.forEach(btn => {
+      expect(btn.attributes().disabled).toBeDefined();
+    });
+  });
+
+  test("Щелчок по кнопке отправляет событие", async () => {
+    wrapper = mountWrapper({
+      component, props: {
+        year: 2022,
+        min: 2021,
+        max: 2023
+      }
+    });
+    const buttons = wrapper.findAll(".v-btn");
+    await buttons[0].trigger("click");
+    expect(wrapper.emitted().decrement).toBeDefined();
+
+    await buttons[1].trigger("click");
+    expect(wrapper.emitted().increment).toBeDefined();
+  });
+});
