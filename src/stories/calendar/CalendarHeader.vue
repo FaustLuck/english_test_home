aabcb2203731d79ee29acdb55d111cfbcb1a4d5a
faustluck <faustluck91@gmail.com>
2023-04-18 02:29:45 +0300

    calendar header

--- src/stories/calendar/CalendarHeader.vue
+++ src/stories/calendar/CalendarHeader.vue
@@ -1,11 +1,13 @@
 <template>
   <v-card class="d-flex justify-center align-center mb-3" elevation="5" color="transparent">
-    <v-btn icon="mdi mdi-chevron-left" elevation="5" color="transparent" @click="$emit('decrement')"/>
+    <v-btn icon="mdi mdi-chevron-left" elevation="5" color="transparent" @click="$emit('decrement')"
+           :disabled="year<=props.min"/>
     <v-card-title>{{ year }}</v-card-title>
-    <v-btn icon="mdi mdi-chevron-right" elevation="5" color="transparent" @click="$emit('increment')"/>
+    <v-btn icon="mdi mdi-chevron-right" elevation="5" color="transparent" @click="$emit('increment')"
+           :disabled="year>=props.max"/>
   </v-card>
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{ year: number }>();
+const props = defineProps<{ year: number, min: number,max:number }>();
 </script>
