e3236d3718e5ef3a640c77150be4cefef287f8c8
faustluck <faustluck91@gmail.com>
2023-04-18 14:35:45 +0300

    add year param

--- src/pages/UsersView.vue
+++ src/pages/UsersView.vue
@@ -36,7 +36,7 @@ const { sub } = storeToRefs(useAuthStore());
 
 onMounted(async () => {
   if (!users.value.length) await useHistoryStore().getUsers(sub.value);
-  if (users.value.length === 1) await router.replace({ name: "history", params: { sub: sub.value } });
+  if (users.value.length === 1) await router.replace({ name: "history", params: { sub: sub.value, year: year.value } });
   useLoadingStore().isLoading = false;
 });
 
