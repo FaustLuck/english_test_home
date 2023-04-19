64ac04b2d4dd22668e218633964c813fbed8416c
faustluck <faustluck91@gmail.com>
2023-04-19 23:21:09 +0300

    result header

--- test/Components/cards/CardResultInfo.test.ts
+++ test/Components/cards/CardResultInfo.test.ts
@@ -50,62 +50,11 @@ describe("CardResultInfo", () => {
     wrapper.unmount();
   });
 
-  test("Отрисовка, если пользователь не авторизован", () => {
-    wrapper = mountWrapper({ component, props: { timestamp, correct: -1 } }, {
-      test: {
-        timeSpent: 155,
-        timeLeft: 25,
-      }
-    });
-    expect(wrapper.text().search("Вход не выполнен") > -1).toBe(true);
-  });
-
-  test("Отрисовка, если пользователь авторизован", () => {
-    wrapper = mountWrapper({ component, props: { timestamp, correct: -1 } }, {
-      test: {
-        timeSpent: 155,
-        timeLeft: 25,
-      },
-      auth: {
-        name: "UserName"
-      }
-    });
-    expect(wrapper.text().search("Вход не выполнен") > -1).toBe(false);
-    expect(wrapper.text().search("UserName") > -1).toBe(true);
-  });
-
-  test("Проверка отрисовка даты и времени тестирования", () => {
-    wrapper = mountWrapper({ component, props: { timestamp, correct: -1 } }, {
-      test: {
-        timeSpent: 155,
-        timeLeft: 25,
-      }
-    });
-    expect(wrapper.text().search("13.04.2023 19:21") > -1).toBe(true);
-    expect(wrapper.text().search("2:35") > -1).toBe(true);
-  });
-
-  test("Отрисовка во время процесса загрузки", () => {
-    wrapper = mountWrapper({ component, props: { timestamp, correct: -1 } }, {
-      test: {
-        timeSpent: 155,
-        timeLeft: 25,
-      }
-    });
-    expect(wrapper.text().search("Loading") > -1).toBe(true);
-    expect(wrapper.text().search(/\?\? \/ \?\?/) > -1).toBe(true);
-  });
-
   test("Отрисовка после загрузки (пропущена сложность 'hard'", () => {
-    wrapper = mountWrapper({ component, props: { timestamp, correct: 5, result } }, {
-      test: {
-        timeSpent: 155,
-        timeLeft: 25,
-      }
-    });
-    expect(wrapper.text().search('easy')>-1).toBe(true);
-    expect(wrapper.text().search('medium')>-1).toBe(true);
-    expect(wrapper.text().search('hard')>-1).toBe(false);
-    expect(wrapper.text().search('5 / 7'))
+    wrapper = mountWrapper({ component, props: { timestamp, correct: 5, result } });
+
+    expect(wrapper.text()).toContain("easy");
+    expect(wrapper.text()).toContain("medium");
+    expect(wrapper.text()).not.toContain("hard");
   });
 });
