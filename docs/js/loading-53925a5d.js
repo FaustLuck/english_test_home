26053ed215a814d317109832fa140eb504ef900f
faustluck <faustluck91@gmail.com>
2023-04-23 13:45:18 +0300

    ver_3 failed fetch dynamic import

--- docs/js/loading-53925a5d.js
+++
@@ -1,1 +0,0 @@
-import{G as a,r}from"./main-ecb08509.js";const t=a("loading",()=>{const o=r(!1);function e(n){o.value=n}return{isLoading:o,setLoading:e}});export{t as u};
--- docs/js/timeToString-e34e66a4.js
+++
@@ -1,1 +0,0 @@
-function e(t){let n=t%60;return`${(t-n)/60}:${n.toString().padStart(2,"0")}`}export{e as t};
