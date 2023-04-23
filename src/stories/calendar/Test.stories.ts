462cde224ac830c55e16be741f70523105e67ffd
faustluck <faustluck91@gmail.com>
2023-04-23 11:17:05 +0300

    upd test story

--- src/stories/calendar/Test.stories.ts
+++ src/stories/calendar/Test.stories.ts
@@ -3,15 +3,27 @@ import Test from "@/stories/calendar/Test.vue";
 import { HistoryRecord } from "@/types/history";
 import { useHistoryStore } from "@/store/history";
 
+const defaultInfo = {
+  "correct": 3,
+  "questions": 4,
+  "timeLeft": 297,
+  "timeSpent": 3
+};
+const failedInfo = {
+  "correct": 3,
+  "questions": 4,
+  "timeLeft": 0,
+  "timeSpent": 3
+};
+const CompletelyTrueInfo = {
+  "correct": 4,
+  "questions": 4,
+  "timeLeft": 297,
+  "timeSpent": 3
+};
 const resultLoaded: HistoryRecord = {
   "key": "key1",
   "timestamp": 1678976520000,
-  "info": {
-    "correct": 3,
-    "questions": 4,
-    "timeLeft": 297,
-    "timeSpent": 3
-  },
   "test": {
     "easy": [
       {
@@ -44,8 +56,9 @@ const meta = {
   args: {
     sub: "sub",
     timestamp: "1678976520000",
-    result: resultLoaded
+    result: Object.assign(resultLoaded, { info: defaultInfo })
   },
+  tags: ["autodocs"],
   render: (args: { sub: string, timestamp: string, result: HistoryRecord }) => ({
     components: { Test },
     setup() {
@@ -63,3 +76,15 @@ export default meta;
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {};
+
+export const Failed: Story = {
+  args: {
+    result: Object.assign({}, resultLoaded, { info: failedInfo })
+  }
+};
+
+export const CompletelyTrue: Story = {
+  args: {
+    result: Object.assign({}, resultLoaded, { info: CompletelyTrueInfo })
+  }
+};
